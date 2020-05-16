bec636f92069ca070cac4338b841ae6a87453ef6
和藤仁 <wtjn1949@gmail.com>
2020-05-16 16:41:45 +0900

    no message

--- scroll_smly.js
+++ scroll_smly.js
@@ -1,10 +1,25 @@
-$(function(){
-   $('a[href^=#]').click(function() {
-      var href= $(this).attr("href");
+$(function () {
+   $('a[href^="#"]').click(function () {
+      var speed = 500;
+      var href = $(this).attr("href");
       var target = $(href == "#" || href == "" ? 'html' : href);
       var position = target.offset().top;
-      var speed = 500; // スクロールの速度（ミリ秒）
-      $('body,html').animate({scrollTop:position}, speed, 'swing'); // スクロール方式
+      $("html, body").animate({ scrollTop: position }, speed, "swing");
       return false;
    });
 });
+
+$(function () {
+   $(window).scroll(function () {
+      $('.fadein').each(function () {
+         var position = $(this).offset().top;
+         var scroll = $(window).scrollTop();
+         var windowHeight = $(window).height();
+         if (scroll > position - windowHeight + 200) {
+            $(this).addClass('active');
+         }
+      });
+   });
+});
+
+$('h2').fadeIn(3000);
